b4331a1452d67e1739ada27fd4db502f105076be
Chris2022 <ctejada415gmail.com>
2021-10-13 21:31:41 -0400

    updated pages and files

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,8 +2,50 @@
   <section class="section">
     <h1 class="title">About Fit Bud</h1>
     <h2 class="subtitle">
-      Fit-Bud helps your journey to either weight loss, muscular gains, or just live a healthier life style.
+      Here at Fit-Bud our goal is to help you reach your fitness goals.
+      Whether you're here to track calories, mileage, track your progress in muscle mass, 
+      track your weight loss/gain, we strive to help you be the best you !
     </h2>
+    <h2 class="subtitle">
+        <strong>What is Fit-Bud?</strong>
+        <p class="box">
+          Fit-Bud is a fitness tracke application that is designed to
+          help you in your fitness journey. Whether you are looking to increase
+          muscle or loose that stubborn belly fat, Fit-Bud is with you all the way,
+          keeping track of everything so you don't have too.
+        </p>
+    </h2>
+    <h2 class="subtitle">
+        <strong>What People Love Fit-Bud</strong>
+        <p class="box">
+          The simple layout and easy to use interface allows anyone of any ages
+          to be able to use Fit-Bud. Because of this, Fit-Bud has become very
+          popular !
+        </p>
+    </h2>
+      <h2 class="subtitle">
+        <strong>Why Choose us over others?</strong>
+        <p class ="box">
+          Other fitness tracker apps overcomplicate things and require you to have to pay
+          in order to get premium benefits. Fit-Bud is very simple to use and navigate. It
+          also gives you a lot of premium features you would have to pay for in other 
+          fitness applications.
+        </p>
+    </h2>
+       <h2 class="subtitle">
+        <strong>Simple Yet Powerful</strong>
+        <p class ="box">
+          Being simple doesn't mean you have to compromise! 
+          Fit-Bud Log has many features others don't have such as a 
+          summary page with stats from your previous workout, powerful graphing of 
+          both strength and cardio exercises, the ability to log supersets, and much more.
+        </p>
+    </h2>
+    <h1 class="title">Reviews</h1>
+    <h2 class="subtitle">Check what people have to say about Fit-Bud</h2>
+    <div class="box">
+      <p>"Fit-Bud is so easy to use!"</p>
+    </div>
 </section>
 </template>
 
