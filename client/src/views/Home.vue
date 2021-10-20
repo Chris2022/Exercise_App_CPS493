a54f509b4c8e3e6de6942ad88a234b2cc303540c
Chris2022 <ctejada415gmail.com>
2021-10-20 18:07:11 -0400

    Updates

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -7,11 +7,11 @@
             <strong>Take</strong> control of your goals.<br> 
             <strong>Track</strong> calories. <br>
             <strong>Break down</strong> ingredients.<br>
-            <strong>Log activities</strong><br> 
-            All with <strong>Fit Bud</strong>.
+            <strong>Log </strong>activities<br> 
+            All possible with <strong>Fit Bud</strong>.
         </h2><br>
         <figure>
-            <img src="https://media.istockphoto.com/photos/calories-counting-diet-food-control-and-weight-loss-concept-calorie-picture-id1259984250?b=1&k=20&m=1259984250&s=170667a&w=0&h=vGSS9ZnaRPgR90BCxVz_mgWePp9cfwH6U1KAu1_as-c=" width="500" height="600" class="center" alt="picture of food">
+            <img src="https://media.istockphoto.com/photos/calories-counting-diet-food-control-and-weight-loss-concept-calorie-picture-id1259984250?b=1&k=20&m=1259984250&s=170667a&w=0&h=vGSS9ZnaRPgR90BCxVz_mgWePp9cfwH6U1KAu1_as-c=" width="1000" height="1000" class="center" alt="picture of food">
         </figure>
     </section>
 </template>
