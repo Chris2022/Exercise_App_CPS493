c13fecb56d36f56689ccaa552c2e534f7832531b
Chris2022 <ctejada415gmail.com>
2021-11-08 18:17:59 -0500

    Changes to Progress.vue

--- client/src/views/Progress.vue
+++ client/src/views/Progress.vue
@@ -4,20 +4,37 @@
         <h2 class="subtitle">
             View your daily progress here!     
         </h2>
-        <div class="field">
+        <div class="box">
+            <h2 class="subtitle">
+                Submit Your Daily Calorie and Exercise Goal!
+            </h2>
+            <form>
+                <label class="label">Daily Calories (Cal):</label>
+                <input type="number" class="input is-normal" v-model="totalCal">
+                <label class="label">Daily Exercise (Min):</label>
+                <input type="number" class="input is-normal" v-model="totalExercise">
+                <button class="button is-link is-hovered" @click.prevent="update(totalCal,totalExercise)">Daily Goal!</button>
+                <button class="button is-light is-hovered" @click.prevent="clear()">Clear</button>
+
+            </form>
+        </div>
+        <div class="box">
             <h2 class="subtitle">Progress on : {{ date }} at {{ time }}</h2>
-        </div>
-        <div class="field">
             <label class="label">Enter calories burned:</label>
-            <input type= "number" v-model="calories">
+            <input class="input is-normal" type= "number" v-model="enegry"><br>
+            <button class="button is-link is-hovered" @click.prevent="updateCalories(enegry)">Log Calories!</button>
+            <button class="button is-light is-hovered" @click.prevent="clear()">Clear</button>
+            <p><strong>Daily Calorie Goal: {{total_calories}} Cal </strong></p>
+            <progress class="progress is-link is-large" :value="this.calories" :max="this.total_calories"></progress>
+            <p>Total Calories Burned: {{this.calories}}cal</p>
+
             <label class="label">Enter total exercise: </label>
-            <input type="number" v-model="exercise">
-        </div>
-        <div>
-            <p><strong>Daily Calorie Goal:700 Cal </strong></p>
-            <progress class="progress is-info" value="75" max="100"></progress>
-            <p><strong>Daily Exercise: 30 min</strong></p>
-            <progress class="progress is-info" value="75" max="100"></progress>
+            <input class="input is-normal" type="number" v-model="fitness"><br>
+            <button class="button is-link is-hovered" @click.prevent="updateExercise(fitness)">Log Exercise!</button>
+            <button class="button is-light is-hovered" @click.prevent="clear()">Clear</button>
+            <p><strong>Daily Exercise: {{total_exercise}}min</strong></p>
+            <progress class="progress is-link is-large" :value="this.exercise" :max="this.total_exercise">{{this.exercise}}%</progress>
+            <p>Total Exercise: {{this.exercise}}min</p>
         </div>
     </section>
 </template>
@@ -27,17 +44,52 @@ export default {
 data: () => ({
   date: "",
   time: "",
-  value:0,
+  total_calories:0,
+  total_exercise:0,
+  calories: 0,
+  exercise:0,
 }),
 methods: {
-    // getValue(){
-    // },
+    update(temp1,temp2){
+        this.total_calories = temp1;
+        this.total_exercise = temp2;
+    },
+    updateExercise(val){
+        if(this.exercise >= 60){
+            this.$oruga.notification.open({
+            message: 'You have Hit Your Daily Exercise Goal!',
+            variant: 'info',
+            position: 'top',
+            closable: true
+        })
+        }else{
+           return this.exercise += val;
+        }
+    },
+    updateCalories(val){
+        if(this.calories >= 1000){
+            this.$oruga.notification.open({
+            message: 'You have hit your daily calories!',
+            variant: 'info',
+            position: 'top',
+            closable: true
+        })
+        }else{
+            return this.calories += val;
+        }
+    },
     getDate: function () {
         return new Date().toLocaleDateString();
     },
     getTime: function () {
         return new Date().toLocaleTimeString();
     },
+    clear(){
+        this.calories =0;
+        this.exercise = 0;
+        this.total_calories=0;
+        this.total_exercise=0;
+    }
 },
 mounted: function () {
   this.date = this.getDate();
@@ -47,4 +99,8 @@ mounted: function () {
 </script>
 
 <style>
+.button {
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
 </style>
