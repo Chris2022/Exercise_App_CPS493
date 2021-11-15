f61f8234f928f7d49ffa5e165f4d60c10c8fb507
Chris2022 <ctejada415gmail.com>
2021-11-14 20:40:15 -0500

    ran build

--- client/src/services/posts.js
+++ client/src/services/posts.js
@@ -1,18 +1,13 @@
 import { api } from "./myFetch";
-
 export function GetAll() {
     return api('posts');
 }
-
 export function GetWall(handle) {
     return api('posts/wall/' + handle);
 }
-
 export function GetFeed(handle) { 
     return api('posts/feed/' + handle);   
 }
-
-
 export function Get(post_id) { return api('posts/' + post_id); }
 export function Add(post) {
      return { ...post };
