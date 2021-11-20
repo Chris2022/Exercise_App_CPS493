bd8bde7279e0e13332f5402eebd06788b25c8191
Chris2022 <ctejada415gmail.com>
2021-11-19 19:14:04 -0500

    bug fixes

--- client/src/views/activity.vue
+++ client/src/views/activity.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="section">
-      <h1 class="title"> Feed Page </h1>
+      <h1 class="title animate__animated animate__zoomIn"> Feed Page </h1>
       <div class="columns">
         <div class="column is-half is-offset-one-quarter">
+            
             <post-edit :new-post="newPost" @add="add()" />
             <div class="post" v-for=" (p, i) in posts" :key="p.src">
                 <post :post="p" @remove="remove(p, i)" />
@@ -34,20 +35,19 @@ export default {
     }),
     async mounted(){
         this.posts = await GetFeed(session.user.handle)
+        for(let i =0; this.posts.length; i++){
+            console.log(this.posts[i]);
+        }
     },
     methods: {
         async remove(post, i){
-            console.log({post})
             const response = await Delete(post._id)
             if(response.deleted){
                 this.posts.splice(i, 1)
             }
         },
         async add(){
-            console.log("Adding new post at " + new Date())
             const response = await Add(this.newPost);
-            console.log({ response });
-
             if(response){
                 this.posts.unshift(response);
                 this.newPost = newPost();
